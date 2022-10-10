40fa0bc9ac0d1315e8cc891ea433675a684f4c51
Ebuube <onwutaebubegideon1555@gmail.com>
2022-10-10 08:43:10 +0100

    Established this repo, created the necessary introductory files

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,11 @@
+```
+ONWUTA EBUBE GIDEON
+```
+## Description of the files in this directory
+
+The file dog.h is a C header file that defines a new type struct dog with the following elements:
+
+name, type = char *
+age, type = float
+owner, type = char *
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - data concerning a dog
+ * @name: the name of the dog
+ * @owner: the owner of the dog
+ * @age: how long the dog has been alive
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
