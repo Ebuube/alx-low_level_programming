68dd99e6c32f8b8664bf56a4cf59382f492324a1
Ebuube <onwutaebubegideon1555@gmail.com>
2022-10-11 03:59:11 +0100

    Corrected the nil printing ability of the file 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,10 +9,17 @@
  */
 void print_dog(struct dog *d)
 {
+	int test = 0;
+
 	if (d != 0)
 	{
-		printf("Name: %s\n", d->name);
+		test = (d->name[0] != '\0' && d->name != 0) ?
+			printf("Name: %s\n", d->name) :
+			printf("Name: nil\n");
 		printf("Age: %.6f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		test = (d->owner[0] != '\0' && d->owner != 0) ?
+		printf("Owner: %s\n", d->owner) :
+		printf("Owner: nil\n");
+		test = test - 2;
 	}
 }
