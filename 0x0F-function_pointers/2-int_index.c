198484bb744ed2433cf1948fe8579c8cf2ef68bc
Ebuube <onwutaebubegideon1555@gmail.com>
2022-10-14 00:51:56 +0100

    Modified the function in 2-int_index.c to check the passing of null pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -20,11 +20,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	}
 
-	for (i = 0; i < size; i++)
+	if (array != 0 && cmp != 0)
 	{
-		if (cmp(array[i]) != 0)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
 		}
 	}
 
