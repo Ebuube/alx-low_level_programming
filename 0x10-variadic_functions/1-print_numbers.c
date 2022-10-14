ae9e2e1828695bec636cbd0b55bddd9b49e8c9c7
Ebuube <onwutaebubegideon1555@gmail.com>
2022-10-14 12:27:14 +0100

    Corrected teh printing of next line by using only printf()

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,7 +5,7 @@
 /**
  * print_numbers - prints the numbers sent to it, separated by a separator
  * @separator: string to be placed in between the numbers
- * @n: the number of items passedd
+ * @n: the number of items passed
  *
  * Description: if separator is NULL, don't print it.
  * Print a new line at the end of the function.
@@ -14,7 +14,7 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list my_list;
-	int i = 0;
+	unsigned int i = 0;
 
 	if (n == 0)
 	{
@@ -24,10 +24,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; (const unsigned int) i < n; i++)
 	{
 		printf("%d", va_arg(my_list, int));
-		if (separator != 0 &&
-				(const unsigned int) i < n - 1)
+		if ((separator != 0 && separator[0] != '\0')
+				&& (const unsigned int) i < n - 1)
 			printf("%s", separator);
 	}
 	va_end(my_list);	/* important */
-	putchar('\n');
+	printf("\n");
 }
