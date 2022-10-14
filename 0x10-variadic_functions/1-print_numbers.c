6924629a8dc3139af4a8116aeb9a8eb6a08a7bfa
Ebuube <onwutaebubegideon1555@gmail.com>
2022-10-14 04:38:19 +0100

    Created the file 1-print_numbers.c and edited README.md

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+#include <stdio.h>
+
+/**
+ * print_numbers - prints the numbers sent to it, separated by a separator
+ * @separator: string to be placed in between the numbers
+ * @n: the number of items passedd
+ *
+ * Description: if separator is NULL, don't print it.
+ * Print a new line at the end of the function.
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list my_list;
+	int i = 0;
+
+	if (n == 0)
+	{
+		return;
+	}
+	va_start(my_list, n);
+	for (i = 0; (const unsigned int) i < n; i ++)
+	{
+		printf("%d", va_arg(my_list, int));
+		if (separator != 0 &&
+				(const unsigned int) i < n - 1)
+			printf("%s", separator);
+	}
+	putchar('\n');
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -9,3 +9,12 @@ The file 0-sum_them_all.c contains a C function that returns the sum of all its 
 Prototype: int sum_them_all(const unsigned int n, ...);
 If n == 0, return 0
 
+The file 1-print_numbers.c contains a C function that prints numbers, followed by a new line.
+
+Prototype: void print_numbers(const char *separator, const unsigned int n, ...);
+where separator is the string to be printed between numbers
+and n is the number of integers passed to the function
+You are allowed to use printf
+If separator is NULL, don’t print it
+Print a new line at the end of your function
+
