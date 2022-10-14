dddac37e2cb277ce502249e3ac6dd784437d5e3f
Ebuube <onwutaebubegideon1555@gmail.com>
2022-10-14 15:47:53 +0100

    Corrected documentation

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_string - prints strings, followed by a new line
+ * print_strings - prints strings, followed by a new line
  * @separator: what to use to separate the strings
  * @n: the number of strings passed
  *
