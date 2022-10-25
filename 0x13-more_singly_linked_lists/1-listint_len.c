572ca7aa4bd84bf75c0a406db267a5d8ad842cd8
Ebuube <onwutaebubegideon1555@gmail.com>
2022-10-25 21:20:13 +0100

    Created the file 1-listint_len.c and updated README.md

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - calculates the number of elements in a linked listint_t list
+ * @head: points to head of list
+ *
+ * Return: the number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *tmp = h;
+	size_t count = 0;
+
+	for (count = 0; tmp != 0; count++)
+		tmp = tmp->next;
+
+	return (count);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -13,3 +13,7 @@ Return: the number of nodes
 Format: see example
 You are allowed to use printf
 
+The file 1-listint_len.c contains a C function that returns the number of elements in a linked listint_t list.
+
+Prototype: size_t listint_len(const listint_t *h);
+
