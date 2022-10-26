bfcffac50eb825b8e1d5fe0f1244c73997086ce6
Ebuube <onwutaebubegideon1555@gmail.com>
2022-10-27 00:22:08 +0100

    Modified the file 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,8 +11,11 @@ size_t print_listint(const listint_t *h)
 	const listint_t *tmp = h;
 	size_t count = 0;
 
-	for (count = 0; tmp != 0; tmp = tmp->next, count++)
+	for (count = 0; tmp != 0; count++)
+	{
 		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
 
 	return (count);
 }
