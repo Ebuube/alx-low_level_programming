32d99e38f52f84e7dd2aab9c90334ac8e098de02
Ebuube <onwutaebubegideon1555@gmail.com>
2022-10-26 01:55:17 +0100

    Modified the file 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,7 +12,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *tmp = 0, *mov = 0;
 	int i = 0;
 
-	/* create and initialize node */
 	tmp = malloc(sizeof(list_t));
 	if (tmp == 0)
 	{
@@ -20,7 +19,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	if (str == 0 || str[0] == '\0')
 	{
-		tmp->str = 0;
+		tmp->str = strdup("");
+		if (!tmp->str)
+		{
+			free(tmp), tmp = NULL;
+			return (NULL);
+		}
 		tmp->len = 0;
 	}
 	else
@@ -28,25 +32,19 @@ list_t *add_node_end(list_t **head, const char *str)
 		tmp->str = strdup(str);
 		if (tmp->str == 0)
 		{
-			free(tmp);
-			tmp = 0;
+			free(tmp), tmp = 0;
 			return (0);
 		}
 		for (tmp->len = 0; tmp->str[i] != '\0'; i++)
 			tmp->len++;
 	}
-	/* insert node */
 	if ((*head) != 0)
 	{
 		for (mov = (*head); mov->next != 0; mov = mov->next)
 			;
-		mov->next = tmp;
-		tmp->next = 0;
+		mov->next = tmp, tmp->next = 0;
 	}
 	else
-	{
-		tmp->next = 0;
-		(*head) = tmp;
-	}
+		tmp->next = 0, (*head) = tmp;
 	return (tmp);
 }
