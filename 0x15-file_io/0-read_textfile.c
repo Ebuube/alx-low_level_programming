b0314617857a70ee384491760d1bcf90022631e7
Ebuube <onwutaebubegideon1555@gmail.com>
2022-11-08 00:48:46 +0100

    Corrected some betty errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
+#include <stdio.h>
 #include <unistd.h>
 
 /**
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -41,7 +41,7 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 
-	close (fd_write);
+	close(fd_write);
 
 	return (1);
 }
