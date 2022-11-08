89e089b4f0886f160457360a1fb4d41a8dd8e165
vagrant <vagrant@vagrant.vm>
2022-11-08 01:22:59 +0000

    Created the file 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,78 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define UNUSED __attribute__((unused))
+#define BUFSIZE 1024
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Description: if incorrect number of arguments, exits with code 97
+ * if file_to already exists, it will be truncated
+ * if file_from doesn't exist or can't be read from, exits with code 98
+ * if file_to cannot be created or written to, exits with code 99
+ * if a file descriptor could not be closed, exits with code 100
+ * Permissions of the created file: rw-rw-r--.
+ * if file_to exist, the permissions will not be changed
+ * Return: 0 if successfull
+ */
+int main(UNUSED int argc, UNUSED char *argv[])
+{
+	char buffer[BUFSIZE];
+	int fd_from = 0, fd_to = 0, bytes = 0;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can\'t read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can\'t write to %s\n", argv[2]);
+		exit(99);
+	}
+	while (1)
+	{
+		bytes = read(fd_from, buffer, BUFSIZE);
+		if (bytes == 0)
+		{
+			break;
+		}
+		else if (bytes == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can\'t read from file %s\n", argv[1]);
+			exit(98);
+		}
+		bytes = write(fd_to, buffer, bytes);
+		if (bytes == 0 || bytes == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can\'t write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can\'t close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can\'t close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
