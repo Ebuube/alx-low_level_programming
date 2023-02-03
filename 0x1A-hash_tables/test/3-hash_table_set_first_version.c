11d1a945b7d5e06aa26b4a59e180a52bb9f953f4
Onwuta Ebube Gideon <onwutaebubegideon1555@gmail.com>
2023-02-03 22:40:51 +0000

    solved task #3

--- 0x1A-hash_tables/test/3-hash_table_set_first_version.c
+++ 0x1A-hash_tables/test/3-hash_table_set_first_version.c
@@ -45,7 +45,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = new_node;	/* first item in the list */
 	else
 	{
-		free(ht->array[index]);
+		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 	}
 	return (1);
