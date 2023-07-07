d2c839f2f3635db9233456b666d8409db95645e9
Ebuube <onwutaebubegideon1555@gmail.com>
2023-07-07 17:16:23 +0000

    Task 2. key -> index: a function that gives index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - calculates the index of a key
+ * @key: the key to hash
+ * @size: the size of the array of the hash table
+ *
+ * Return: the index at which the key/value pair should be stored in the array
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
